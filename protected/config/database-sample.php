5a6dc06c2db187706401d8f97f639e2c39c6e9a7
Nur Fadillah Fajar <n.fadillahfajar@gmail.com>
2016-05-29 15:41:42 +0700

    setup base config

--- protected/config/database-sample.php
+++ protected/config/database-sample.php
@@ -2,13 +2,13 @@
 
 // This is the database connection configuration.
 return array(
-	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	// 'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-	/*
-	'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+
+	'connectionString' => 'pgsql:host=localhost;dbname=your_db_name',
 	'emulatePrepare' => true,
-	'username' => 'root',
-	'password' => '',
+	'username' => 'your_db_user',
+	'password' => 'your_db_password',
 	'charset' => 'utf8',
-	*/
+
 );
