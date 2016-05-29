edc758434e6f25686f5de6414d34b1aaacf43f62
Nur Fadillah Fajar <n.fadillahfajar@gmail.com>
2016-05-29 20:25:37 +0700

    add crud for user and role + setup rbac controllers

---
+++ protected/views/user/_view.php
@@ -0,0 +1,51 @@
+<?php
+/* @var $this UserController */
+/* @var $data User */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
+	<?php echo CHtml::encode($data->name); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('username')); ?>:</b>
+	<?php echo CHtml::encode($data->username); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('email')); ?>:</b>
+	<?php echo CHtml::encode($data->email); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('password')); ?>:</b>
+	<?php echo CHtml::encode($data->password); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('role_id')); ?>:</b>
+	<?php echo CHtml::encode($data->role_id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('address')); ?>:</b>
+	<?php echo CHtml::encode($data->address); ?>
+	<br />
+
+	<?php /*
+	<b><?php echo CHtml::encode($data->getAttributeLabel('phone')); ?>:</b>
+	<?php echo CHtml::encode($data->phone); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('profile_picture')); ?>:</b>
+	<?php echo CHtml::encode($data->profile_picture); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('is_active')); ?>:</b>
+	<?php echo CHtml::encode($data->is_active); ?>
+	<br />
+
+	*/ ?>
+
+</div>
